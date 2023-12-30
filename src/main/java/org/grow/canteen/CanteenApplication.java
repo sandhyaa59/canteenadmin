ac7f991e3ee2bd5fd2104cc11817c2f6810ad557
sandip125 <sandiptiwari633@gmail.com>
2023-12-30 12:14:17 +0545

    PORT

--- src/main/java/org/grow/canteen/CanteenApplication.java
+++ src/main/java/org/grow/canteen/CanteenApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CanteenApplication {
-
+public String PORT=System.getenv("PORT");
     public static void main(String[] args) {
         SpringApplication.run(CanteenApplication.class, args);
     }
