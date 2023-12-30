63401c3f1873a7bf5ff9bc3309eeaaee6ac5ce38
sandip125 <sandiptiwari633@gmail.com>
2023-12-30 11:40:04 +0545

    updated with Equal and hashcode

--- src/main/java/org/grow/canteen/dto/pageableDto/PageableDataWithObject.java
+++ src/main/java/org/grow/canteen/dto/pageableDto/PageableDataWithObject.java
@@ -1,10 +1,12 @@
 package org.grow.canteen.dto.pageableDto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.data.domain.Page;
 
 import java.util.Map;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class PageableDataWithObject<T> extends PageableData<T>{
     private Map<String, Object> object;
